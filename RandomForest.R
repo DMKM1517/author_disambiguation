ca5d94444b6adbb23c06a792f179c60f4ebcffc7
Rodney <reln13st@gmail.com>
2016-03-20 19:03:39 +0100

    confusion matrix of random forest

--- RandomForest.R
+++ RandomForest.R
@@ -1,8 +1,11 @@
 
 # install.packages("RPostgreSQL")
 # install.packages("randomForest")
+# install.packages("caret")
+# install.packages("e1071")
 require("RPostgreSQL")
 library(randomForest)
+library(caret)
 
 ######### CONNECTION TO DB ###############
 
@@ -35,11 +38,26 @@ df_distances <- dbGetQuery(con, query_distances)
 head(df_distances, n = 10)
 dim(df_distances)
 
+# id as row name
 df_bin <- data.frame(df_distances[,-1], row.names=df_distances[,1])
 head(df_bin, n = 10)
 
-df_x <- df_bin[,1:6]
-df_y <- as.factor(df_bin$same_author)
+# divide training and testing sets
+bound <- floor((nrow(df_bin)/4)*3)                  #define % of training and test set
+df_sampled <- df_bin[sample(nrow(df_bin)), ]        #sample rows 
+df.train <- df_sampled[1:bound, ]                   #get training set
+df.test <- df_sampled[(bound+1):nrow(df_sampled), ] #get test set
 
-rf <- randomForest(df_x,df_y)
-rf
+# separate x and y
+df_x.train <- df.train[,1:6]
+df_y.train <- as.factor(df.train$same_author)
+df_x.test <- df.test[,1:6]
+df_y.test <- as.factor(df.test$same_author)
+
+# random forest
+rf <- randomForest(df_x.train, df_y.train, df_x.test, df_y.test)
+rf$confusion
+head(rf$predicted, n=10)
+head(rf$test$predicted, n=20)
+
+confusionMatrix(rf$test$predicted, df_y.test)
