446b8ce5a7330be7c18c351f5b177adf6376831e
Jamp <jampmil@yahoo.com>
2016-06-27 22:19:12 -0500

    Fix to scape string in JS

--- web/common/models/article.js
+++ web/common/models/article.js
@@ -13,10 +13,10 @@ module.exports = function(Article) {
 		} else if (!article.references || article.references.length < 1) {
 			cb(new Error('References are missing'));
 		} else {
-			let title = real_escape_string(article.title),
-				journal = real_escape_string(article.journal) || '',
-				year = real_escape_string(article.year) || null,
-				doi = real_escape_string(article.doi) || '',
+			let title = _escapeString(article.title),
+				journal = _escapeString(article.journal) || '',
+				year = _escapeString(article.year) || null,
+				doi = _escapeString(article.doi) || '',
 				query = `
 				select greatest(900000, max(id))+1 as id from source.articles;
 				select max(processid)+1 as process_id from source.articles`
@@ -31,29 +31,29 @@ module.exports = function(Article) {
 						values (${process_id}, ${id}, '${title}', '${journal}', null, '${doi}', ${year});
 						`;
 					for (let i in article.authors) {
-						let author = real_escape_string(article.authors[i]);
+						let author = _escapeString(article.authors[i]);
 						query += `
 							insert into source.signatures 
 							values (${id}, ${i}, '${author.first_name}', '${author.first_name.substr(0,1)}', '${author.middle_name.substr(0,1)}', '${author.last_name}', '${author.middle_name}');
 							`;
 					}
 					for (let keyword of article.keywords) {
-                        keyword_clean = real_escape_string(keyword)
+                        keyword_clean = _escapeString(keyword)
 						query += `
 							insert into source.keywords
 							values (${id}, 'WEB', '${keyword_clean}');
 							`;
 					}
 					for (let subject of article.subjects) {
-                        subject_clean = real_escape_string(subject)
+                        subject_clean = _escapeString(subject)
 						query += `
 							insert into source.subjects
 							values (${id}, '${subject_clean}');
 							`;
 					}
 					for (let reference of article.references) {
-                        ref_journal_clean = real_escape_string(reference.journal)
-                        ref_title_clean = real_escape_string(reference.title)
+                        ref_journal_clean = _escapeString(reference.journal)
+                        ref_title_clean = _escapeString(reference.title)
 						query += `
 							insert into source."references" (id, journal, title)
 							values (${id}, '${ref_journal_clean}', '${ref_title_clean}');
@@ -81,28 +81,30 @@ module.exports = function(Article) {
 			type: 'number'
 		}
 	});
-    function real_escape_string (str) {
-        return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
-            switch (char) {
-                case "\0":
-                    return "\\0";
-                case "\x08":
-                    return "\\b";
-                case "\x09":
-                    return "\\t";
-                case "\x1a":
-                    return "\\z";
-                case "\n":
-                    return "\\n";
-                case "\r":
-                    return "\\r";
-                case "\"":
-                case "'":
-                case "\\":
-                case "%":
-                    return "\\"+char; // prepends a backslash to backslash, percent,
-                                      // and double/single quotes
-            }
+    var _escapeString = function (val) {
+        val = val.replace(/[\0\n\r\b\t\\'"\x1a]/g, function (s) {
+        switch (s) {
+          case "\0":
+            return "\\0";
+          case "\n":
+            return "\\n";
+          case "\r":
+            return "\\r";
+          case "\b":
+            return "\\b";
+          case "\t":
+            return "\\t";
+          case "\x1a":
+            return "\\Z";
+          case "'":
+            return "''";
+          case '"':
+            return '""';
+          default:
+            return "\\" + s;
+        }
         });
-    }
+
+        return val;
+    };
 };
